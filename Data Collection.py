71442222c290d7e3510cc4fb40a248f56e5d2ae3
Sandeepan Paul <sandeepanpaul.tsk@gmail.com>
2020-05-28 13:54:47 +0530

    uploaded scraper and run code

---
+++ Data Collection.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 28 12:37:33 2020
+
+@author: Sandeepan Paul
+"""
+
+import glassdoor_scraper as gs
+import pandas as pd
+path = "D:/DScience/MachineLearningPractical/SalaryProj/chromedriver"
+
+df = gs.get_jobs('data scientist', 15, False, path, 30)
